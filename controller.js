90b2063d52d3d3b204aace19d814103e2ebf0877
yspwf <846163966@qq.com>
2018-09-27 19:45:04 +0800

    数据页面

--- controller.js
+++ controller.js
@@ -3,6 +3,105 @@ const mongoose = require('mongoose');
 const mogo_connect = require('./mongoose.js');
 
 
+function check(str){
+	if(err){
+		console.log(err);
+	}else{
+		console.log('34223');
+	}
+}
+
+/**文章**/
+
+const ArticleShema = new mongoose.Schema({
+	title: {type:String},
+	catogory: {type:Number},
+	tag: {type:Number},
+	views: {type:Number},
+	like: {type:Number},
+	coverImage: {type:String},
+	auther: {type:String},
+	intro: {type:String},
+	content: {type:String},
+	createtime: {type:Date} 
+});
+
+//构建model
+const Article = mongoose.model('Article', ArticleShema, 'article');
+
+
+const ArticleDao = function(){};
+
+
+ArticleDao.prototype.ArticleAdd = function(data){
+		return new Promise((resolve, reject)=>{
+			let obj = new Article(data);
+			obj.save(function(error, doc){
+				if(error){
+					reject(error);
+				}else{
+					resolve(doc);
+				}
+			});
+		});
+};
+
+ArticleDao.prototype.ArticleDel = function(where){
+	return new Promise((resolve, reject)=>{
+		Article.deleteMany(where, function(err, doc){
+			if(err){
+				reject(err);
+			}else{
+				resolve(doc);
+			}
+		});
+		/*Article.remove(where, function(err, doc){
+			if(err){
+				reject(err);
+			}else{
+				resolve(doc);
+			}
+		});*/
+	});
+};
+
+
+ArticleDao.prototype.ArticleList = function(){
+		return new Promise((resolve, reject)=>{
+			Article.find(function(err, docs){
+				if(err){
+					reject(err);
+				}else{
+					resolve(docs);
+				}
+			});
+		});
+}
+
+
+module.exports = new ArticleDao();
+
+
+
+
+/*
+exports.ArticleAction = function(data){
+	return new Promise((resolve, reject)=>{
+		var test = new Article(data);
+		test.save(function(error, doc){
+			 if(error){
+			     reject(error);
+			  }else{
+			     resolve(doc);
+			  }
+		});
+	});
+}
+
+
+
+
+/*
 const userSchema = new mongoose.Schema({
 	name: {type:String},
 	age:  {type:Number, defaut:0}
--- index.js
+++ index.js
@@ -6,11 +6,89 @@ const app = new Koa();
 const KoaRouter = require('koa-router');
 const Router = new KoaRouter();
 
+
+
+/** post数据解析 **/
+const BodyParser = require('koa-bodyparser');
+app.use(BodyParser());
+
+
+/** 时间 moment **/
+const moment = require('moment');
+
+
+/** 引入控制器  **/
+const ArticleController = require('./controller.js');
+
+
 Router.get("/", async (ctx, next)=>{
 	ctx.body="首页";
 })
 
+Router.get("/articlelist", async (ctx, next)=>{
+	let res = await ArticleController.ArticleList();
+	//console.log(res);
+	ctx.body = res;
+});
+Router.post("/articleadd", async (ctx, next)=>{
+	const msg = {};
+	const data = ctx.request.body;
+	if(data.title == ''){
+		msg.success = false;
+		msg.errormsg = "标题不能为空！";
+		ctx.body = msg;
+	}
+	if(data.catogory == ''){
+		msg.success = false;
+		msg.errormsg = "分类不能为空！";
+		ctx.body = msg;
+	}
+	if(data.tag == ''){
+		msg.success = false;
+		msg.errormsg = "标签不能为空！";
+		ctx.body = msg;
+	}
+	if(data.coverImage == ''){
+		msg.success = false;
+		msg.errormsg = "封面不能为空！";
+		ctx.body = msg;
+	}
+	if(data.intro == ''){
+		msg.success = false;
+		msg.errormsg = "简介不能为空！";
+		ctx.body = msg;
+	}
+	if(data.content == ''){
+		msg.success = false;
+		msg.errormsg = "内容不能为空！";
+		ctx.body = msg;
+	}
 
+	data.createtime = moment().format("YYYY-MM-DD HH:mm:ss");
+
+	let res = await ArticleController.ArticleAdd(data);
+	if(res != ''){
+		msg.success = true;
+		msg.errormsg = "";
+		ctx.body = msg;
+	}
+	//console.log(ctx.request.body);
+	//ctx.body ="添加";
+});
+
+
+Router.get("/articledel", async (ctx, next)=>{
+	//console.log(ctx.request.query);
+	const data = ctx.request.query;
+	let res = await ArticleController.ArticleDel(data);
+	console.log(res);
+	//ctx.body ="删除";
+});
+
+
+Router.post("/articleup", async (ctx, next)=>{
+	ctx.body ="修改";
+});
 
 /*
 app.use(async ctx=>{
